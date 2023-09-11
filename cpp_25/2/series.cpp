c160f73f0b3af4cdd74c8864c36599e402691c5c
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2023-09-11 16:13:57 +0530

    Add files via upload

--- cpp_25/2/series.cpp
+++ cpp_25/2/series.cpp
@@ -1,15 +1,24 @@
 //Print the sum of series
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main(){
-	int terms,add;
+	int terms;
+    double add,final;
+    final=0;
 	cout<<"Enter the number of terms";
 	cin>>terms;
 	//for the depiction
-	for (int i=1,i<=terms,i++){
-		cout<<"(1/"<<i<<")^2"<<"\t";
+	for (int i=1;i<=terms;i++){
+		cout<<"(1/"<<i<<")^2 +"<<"\t";
 	}
-	for(int i=1,i<=terms,i++){
-		add=
+    //for the final value
+	for(int i=1;i<=terms;i++){
+		add=sqrt(1/i);
+        final+=add;
+        cout<<add<<"+";
 	}
+
+    cout<<"\t = "<<final;
+    return 0;
 }
