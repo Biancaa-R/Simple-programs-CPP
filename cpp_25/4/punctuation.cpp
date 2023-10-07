893ce0976f7ca4f8c35b42555562e82d453e2d37
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2023-10-07 12:25:18 +0530

    Add files via upload

--- cpp_25/4/punctuation.cpp
+++ cpp_25/4/punctuation.cpp
@@ -1,17 +1,32 @@
 //10.  Write a program to strip the punctuation from a string.
 #include<iostream>
+# include<string>
+# include<stdbool.h>
 using namespace std;
 int main(){
-	string punc[7]={",",".",";",":","-","?","!"};
+	char punc[]={',','.','*','-','_','!'};
 	string value;
+	string new1= "";
+	bool bool_val=false;
 	cout<<"Enter the string with punctuation";
 	cin>>value;
-	for(string pun:punc ){
-		for(string i:value){
+	//getline(cin, value);
+	for(char i:value){
+		bool_val=false;
+		for (char pun:punc){
 			if (i==pun){
-				cout<<"Found a punctuation";
+				cout<<"Found punctuation"<<i<<"\n";
+				bool_val=true;
+				break;
 			}
 		}
+			//new1.append((string)i);
+			if (bool_val==false){
+				new1 +=i;
+				bool_val=false;
+			}
+			
 	}
+	cout<<"The string without punctuation is "<<new1;
 	return 0;
 }
