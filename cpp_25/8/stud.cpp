6d25b9af0f8ab1f0ba986fa47bcc7a24e690bcb5
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2023-12-26 12:52:05 +0530

    Add files via upload

--- cpp_25/8/stud.cpp
+++ cpp_25/8/stud.cpp
@@ -7,17 +7,70 @@ of the marks obtained from the test and the roll_number of students through mult
 using namespace std;
 
 class student{
+    public:
 	int roll_number;
 	string name;
-	public:
+	
 		void s_initialize(){
 			cout<<"Enter the roll number of student";
 			cin>>roll_number;
 			cout<<"Enter the name of the student";
 			cin>>name;
 		}
+
+        void s_diplay()
+        {
+            cout<<"The roll number of student is"<<roll_number<<"\n";
+            cout<<"The name of the student is"<<name<<"\n";
+        }
 };
 
 class test{
+    public:
+    float mark1,mark2,mark3;
+        void t_initialize()
+        {
+            cout<<"Enter the marks1";
+            cin>>mark1;
+            cout<<"Enter the mark2";
+            cin>>mark2;
+            cout<<"Enter the mark3 ";
+            cin>>mark3;
+        }
 	
 };
+
+class result: public test,public student{
+    public:
+        float sum;
+    result()
+    {
+        sum=mark1+mark2+mark3;
+    }
+    void compute_result()
+    {
+        sum=mark1+mark2+mark3;
+    }
+    void r_display()
+    {
+        cout<<"The result of the student is"<<sum<<"\n";
+    }
+};
+
+int main()
+{   
+    int num;
+    cout<<"Enter the number of students\n";
+    cin>>num;
+    result *arr1 = new result[num];
+    for(int i=0;i<num;i++){
+        arr1[i].s_initialize();
+        arr1[i].t_initialize();
+        arr1[i].compute_result();
+    }
+
+    for(int i=0;i<num;i++){
+        arr1[i].s_diplay();
+        arr1[i].r_display();
+    }
+}
