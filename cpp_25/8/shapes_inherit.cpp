5dd638e209196932b796cc566633daebbddd12e7
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2023-12-27 16:10:28 +0530

    Add files via upload

--- cpp_25/8/shapes_inherit.cpp
+++ cpp_25/8/shapes_inherit.cpp
@@ -2,3 +2,63 @@
 //sphere
 //cylinder
 //squares
+
+# include <iostream>
+using namespace std;
+
+class shape{
+
+    public:
+
+    void prints()
+    {
+        cout<<"This is a shape\n";
+    }
+
+};
+
+class sphere: public shape{
+
+    public:
+
+    void print_sphere()
+    {
+        cout<<"This is a sphere \n";
+    }
+};
+
+class cylinder : public shape{
+
+    public:
+
+    void print_cylinder()
+    {
+        cout<<"This is a cylinder\n";
+    }
+};
+
+class squares: public shape{
+
+    public:
+
+    void print_square()
+    {
+        cout<<"This is a square\n";
+    }
+};
+
+int main()
+{
+    sphere s1;
+    s1.prints();
+    s1.print_sphere();
+
+    cylinder c1;
+    c1.prints();
+    c1.print_cylinder();
+
+    squares s2;
+    s2.prints();
+    s2.print_square();
+
+}
